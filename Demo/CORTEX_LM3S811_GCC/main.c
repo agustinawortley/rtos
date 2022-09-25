5e4bde7640d23e1f1a75b7b52787848bbf9baa7b
Agus Wortley <agus.wortley@craftech.io>
2022-09-25 11:37:16 -0300

    top funcionando

--- Demo/CORTEX_LM3S811_GCC/main.c
+++ Demo/CORTEX_LM3S811_GCC/main.c
@@ -68,6 +68,8 @@ char *itoa(int val);
 char* longToChar(unsigned long value, char *ptr, int base);
 static void uartPrint(char * msg,uint8_t len);
 void getTasksStats( char *writeUART );
+void vOwnTaskGetRunTimeStats(signed char *pcWriteBuffer);
+static void to_terminal(char* s);
 
 /* String that is transmitted on the UART. */
 static volatile char *pcNextChar;
@@ -405,14 +407,11 @@ static void vTopTask(void *pvParameters )
 
 	for( ;; )
 	{	
-		//guardo el separador y el encabezado en sus respectivas variubles despues imprimo la salida de vtaskList con formato
-		sprintf(header,"Name Task	State   Priority  Stack  Num");
-		uartPrint( line , strlen(line) );
-		uartPrint( header , strlen(header) );
-		uartPrint( line , strlen(line) );
+		vOwnTaskGetRunTimeStats(pcMessage);
 
-		// getTasksStats(pcMessage);
-		// uartPrint( pcMessage , strlen(pcMessage) );
+		to_terminal(pcMessage);
+
+		vTaskDelay(2000);
 
 		vTaskDelay( pdMS_TO_TICKS(10000) );
 		
@@ -481,3 +480,77 @@ void intToString(int value, char *aux){
 	sprintf(aux, "%d\n", value);
 }
 
+void vOwnTaskGetRunTimeStats(signed char *pcWriteBuffer){
+	TaskStatus_t *pxTaskStatusArray;
+	volatile UBaseType_t uxArraySize, x;
+	uint32_t ulTotalRunTime, ulStatsAsPercentage;
+	char aux[16];
+
+	/* Make sure the write buffer does not contain a string. */
+	*pcWriteBuffer = 0x00;
+
+	/* Take a snapshot of the number of tasks in case it changes while this
+	function is executing. */
+	uxArraySize = uxTaskGetNumberOfTasks();
+
+	/* Allocate a TaskStatus_t structure for each task.  An array could be
+	allocated statically at compile time. */
+	pxTaskStatusArray = pvPortMalloc(uxArraySize*sizeof(TaskStatus_t));
+
+	if(pxTaskStatusArray != NULL){
+		/* Generate raw status information about each task. */
+		uxArraySize = uxTaskGetSystemState( pxTaskStatusArray, uxArraySize, &ulTotalRunTime);
+
+		/* For percentage calculations. */
+		ulTotalRunTime /= 100UL;
+
+		/* Avoid divide by zero errors. */
+		if(ulTotalRunTime > 0){
+			// strcat(pcWriteBuffer, "Name\t\tRunTime\t\t%RunTime\n");
+			strcat(pcWriteBuffer, "-----------------------------\n");
+			/* For each populated position in the pxTaskStatusArray array,
+			format the raw data as human readable ASCII data. */
+			for(x = 0; x<uxArraySize; x++){
+				/* What percentage of the total run time has the task used?
+				This will always be rounded down to the nearest integer.
+				ulTotalRunTimeDiv100 has already been divided by 100. */
+				ulStatsAsPercentage = pxTaskStatusArray[ x ].ulRunTimeCounter / ulTotalRunTime;
+
+				if(ulStatsAsPercentage > 0UL){
+					strcat(pcWriteBuffer, pxTaskStatusArray[x].pcTaskName);
+					strcat(pcWriteBuffer, "\t");
+					strcat(pcWriteBuffer, longToChar(pxTaskStatusArray[ x ].ulRunTimeCounter, aux, 10));
+					strcat(pcWriteBuffer, "\n");
+				}
+				else{
+					/* If the percentage is zero here then the task has
+					consumed less than 1% of the total run time. */
+					strcat(pcWriteBuffer, pxTaskStatusArray[x].pcTaskName);
+					strcat(pcWriteBuffer, "\t");
+					strcat(pcWriteBuffer, longToChar(pxTaskStatusArray[ x ].ulRunTimeCounter, aux, 10));
+					strcat(pcWriteBuffer, "\n");
+				}
+
+				pcWriteBuffer += strlen( ( char * ) pcWriteBuffer );
+			}
+		}
+		/* The array is no longer needed, free the memory it consumes. */
+		vPortFree( pxTaskStatusArray );
+	}
+}
+
+static void to_terminal(char* s){
+	UARTIntDisable(UART0_BASE, UART_INT_TX);
+	{
+		pcNextChar = s;
+
+		/* Send the first character. */
+		if(!(HWREG(UART0_BASE + UART_O_FR) & UART_FR_TXFF)){
+			HWREG(UART0_BASE + UART_O_DR) = *pcNextChar;
+		}
+
+		pcNextChar++;
+	}
+
+	UARTIntEnable(UART0_BASE, UART_INT_TX);
+}
